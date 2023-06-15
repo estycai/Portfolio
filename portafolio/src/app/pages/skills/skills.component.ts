342feb7cfa1dcd095ada9e6893b63c763af638d6
Esteban <estybvb@gmail.com>
2023-06-15 20:34:59 -0300

    skilss terminadas

--- portafolio/src/app/pages/skills/skills.component.ts
+++ portafolio/src/app/pages/skills/skills.component.ts
@@ -11,16 +11,16 @@ export interface Skill {
 })
 export class SkillsComponent implements OnInit {
   habilidades: Skill[] = [
-    { img: 'assets/img/logos/angular.png', title: 'Angular', porcentaje: 70 },
-    { img: 'assets/img/logos/c.png', title: 'C#', porcentaje: 60 },
-    { img: 'assets/img/logos/net.png', title: '.NET', porcentaje: 60 },
-    { img: 'assets/img/logos/sql.png', title: 'SQL', porcentaje: 60 },
-    { img: 'assets/img/logos/html.png', title: 'HTML', porcentaje: 90 },
+    { img: 'assets/img/logos/angular.png', title: 'Angular', porcentaje: 92 },
+    { img: 'assets/img/logos/c.png', title: 'C#', porcentaje: 92 },
+    { img: 'assets/img/logos/net.png', title: '.NET', porcentaje: 92 },
+    { img: 'assets/img/logos/sql.png', title: 'SQL', porcentaje: 80 },
+    { img: 'assets/img/logos/html.png', title: 'HTML', porcentaje: 100 },
     { img: 'assets/img/logos/css.png', title: 'CSS', porcentaje: 80 },
     { img: 'assets/img/logos/js.png', title: 'Javascript', porcentaje: 50 },
-    { img: 'assets/img/logos/java.png', title: 'JAVA', porcentaje: 50 },
+    { img: 'assets/img/logos/java.png', title: 'JAVA', porcentaje: 60 },
     { img: 'assets/img/logos/spring.png', title: 'Spring', porcentaje: 50 },
-    { img: 'assets/img/logos/git.png', title: 'Git', porcentaje: 50 },
+    { img: 'assets/img/logos/git.png', title: 'Git', porcentaje: 85 },
   ];
 
   habilidadesAux:Skill[] = this.habilidades;
